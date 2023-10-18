46c2d078d941d984397e9224cd13dfc2ae4b5290
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2023-10-19 00:26:20 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+import "./App.scss";
+import { tasks } from "./data";
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+function TaskList() {
+  return (
+    <div className="TaskList">
+      <form>
+        <input type="text" placeholder="Enter a task..." />
+      </form>
+      <ul className="Tasks">
+        {tasks.map((task) => (
+          <Task task={task} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Task({ task }) {
+  return (
+    <li className="Task">
+      <div className={`label ${task.completed ? "completed" : ""}`}>
+        {task.title}
+      </div>
+      <input className="checkbox" type="checkbox" checked={task.completed} />
+    </li>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const tasks = [
+  {
+    title: "Do the dished",
+    completed: true,
+  },
+  {
+    title: "Make the bed",
+    completed: false,
+  },
+  {
+    title: "Take a shower",
+    completed: false,
+  },
+];
